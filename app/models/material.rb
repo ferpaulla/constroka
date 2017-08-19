aa4eb9041bc33c5f9eb7b4b009fb8368d83d66e7
Fernanda <ferpaulla@yahoo.com.br>
2017-08-19 17:10:43 -0300

    atualização relacionamentos

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,4 @@
 class Material < ApplicationRecord
+	belongs_to :usuario
+	has_one :troca
 end
--- app/models/troca.rb
+++ app/models/troca.rb
@@ -1,2 +1,9 @@
 class Troca < ApplicationRecord
+	belongs_to :usuario_doador, class_name:"Usuario"
+
+	belongs_to :usuario_receptor, class_name:"Usuario"
+	belongs_to :material
+
+
+
 end
